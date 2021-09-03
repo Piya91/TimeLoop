40390165ab57fcd95799b143697b9b6e8e6b5e54
Piya91 <kaylawaker@icloud.com>
2021-09-03 21:02:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TimeLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x;
+            string msg = "Abracadabra";
+            n = int.Parse(Console.ReadLine());
+            for (x = 1; x <= n; x++)
+                Console.WriteLine(x + " " + msg);
+        }
+    }
+}
